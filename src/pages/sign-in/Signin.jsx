974515dd96384ecdea6ca5203cd8a0e8ae2ca82e
Kevin-tre <kevintrepied@gmail.com>
2024-09-27 12:18:06 +0200

    fix error getUser

--- src/pages/sign-in/Signin.jsx
+++ src/pages/sign-in/Signin.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUser } from '../../redux/action';
-import { loginUserFailed } from '../../redux/reducer';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../redux/action";
+import { loginUserFailed } from "../../redux/reducer";
 import "./signin.css";
 
 const Signin = () => {
   const dispatch = useDispatch();
-  const {error, userLoading} = useSelector(state => state.user); 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { error, userLoading } = useSelector((state) => state.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
 
   const onSubmit = (e) => {
@@ -16,42 +16,53 @@ const Signin = () => {
     const body = { email, password, rememberMe };
 
     if (!email || !password) {
-      dispatch(loginUserFailed()); 
+      dispatch(loginUserFailed());
     }
 
     loginUser(body, dispatch);
   };
 
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
         <form onSubmit={onSubmit}>
           <div className="input-wrapper">
             <label htmlFor="email">E-mail</label>
-            <input 
-              type="text" 
-              id="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input 
-              type="password" 
-              id="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          {error && <p className="message-error">{error}</p>} 
+          <p className="message-error">{error}</p>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" value={rememberMe} onChange={() => setRememberMe(!rememberMe)} />
+            <input
+              type="checkbox"
+              id="remember-me"
+              value={rememberMe}
+              onChange={() => setRememberMe(!rememberMe)}
+            />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button className={userLoading?"my-button-disable":"my-button"} type='submit' disabled={userLoading}>Sign In</button>
+          <button
+            className={userLoading ? "my-button-disable" : "my-button"}
+            type="submit"
+            disabled={userLoading}
+          >
+            Sign In
+          </button>
         </form>
       </section>
     </div>
--- src/router/Navigation.jsx
+++ src/router/Navigation.jsx
@@ -1,23 +1,15 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Footer from "../layout/footer/Footer";
 import Header from "../layout/header/Header";
 import Home from "../pages/Home/Home";
 import Signin from "../pages/sign-in/Signin";
 import User from "../pages/User/User";
-import { getUser } from "../redux/action";
+
 
 
 const Navigation = () => {
-  const dispatch = useDispatch()
   const {token} = useSelector(state => state.user)
-  useEffect(() => {
-    const tokenFromStorage = localStorage.getItem("token")
-    if (tokenFromStorage) {
-      getUser(tokenFromStorage,dispatch)
-    }
-  }, [dispatch])
   return (
     <BrowserRouter>
       <Header />
